800a6e2892927d1bf85f78de056d03996f1f17f4
Kevin Supakkul <kev.supakkul@gmail.com>
2021-05-09 15:17:48 -0500

    first couple states work

--- project_starter_code/controller.cpp
+++ project_starter_code/controller.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <string>
+#include <chrono> // pls
 
 #include <signal.h>
 bool runloop = true;
@@ -22,8 +23,15 @@ const string robot_file = "./resources/mmp_panda.urdf";
 
 #define JOINT_CONTROLLER      0
 #define POSORI_CONTROLLER     1
+//int state = POSORI_CONTROLLER;
 
-int state = JOINT_CONTROLLER;
+void moveTruck(VectorXd q_desired, VectorXd &command_torques, Sai2Model::Sai2Model* &robot);
+void moveArm(VectorXd xd, Matrix3d Rd, VectorXd &command_torques, Sai2Model::Sai2Model* &robot, Vector3d pos_in_link);
+void operationalSpaceMatrices(MatrixXd& Lambda, MatrixXd& Jbar, MatrixXd& N, const MatrixXd& task_jacobian, Sai2Model::Sai2Model* &robot);
+
+typedef enum {INIT, WAIT_FOR_BOX, SCAN_FOR_BOX, OPEN_BOX, GRAB_MAIL, PLACE_MAIL, CLOSE_BOX, RETRACT_ARM} States;
+
+States state = WAIT_FOR_BOX;
 
 // redis keys:
 // - read:
@@ -70,8 +78,11 @@ int main() {
 
 	// pose task
 	const string control_link = "link7";
-	const Vector3d control_point = Vector3d(6.0,0.0,0.07);
+	const Vector3d control_point = Vector3d(0.0,0.0,0.07);
 	auto posori_task = new Sai2Primitives::PosOriTask(robot, control_link, control_point);
+
+    Vector3d p_desired = Vector3d(6, 0, 0);
+    posori_task->_desired_position = p_desired;
 
 #ifdef USING_OTG
 	posori_task->_use_interpolation_flag = true;
@@ -121,7 +132,78 @@ int main() {
 
 		// update model
 		robot->updateModel();
-	
+        switch (state) {
+            case WAIT_FOR_BOX:
+                {
+                    // Action: driving
+                    VectorXd q_desired = initial_q;
+                    q_desired(0) = 6;
+                    moveTruck(q_desired,command_torques,robot);
+
+                    // Trigger: camera detects mailbox aka arrives at desired position
+                    //if ((robot->_q - q_desired).norm() < 1) {
+                    if ((robot->_q - q_desired).norm() < 1 
+                                    && robot->_dq.norm() < 0.01) {
+                        cout << "Truck has arrived!!!!" << endl;
+                        // get coordinates of mailbox using camera
+                        state = SCAN_FOR_BOX;
+                    }
+                    break;
+                }
+            case SCAN_FOR_BOX:
+                {
+
+                cout << "Next: open box!!!!" << endl;
+                state = OPEN_BOX;
+                break;
+                }
+            case OPEN_BOX:
+                {
+                    Vector3d xd = Vector3d(6, 0.5, 0.8);
+                    Matrix3d Rd;
+                    Rd << cos(M_PI/3),0,sin(M_PI/3),0,1,0,-sin(M_PI/3),0,cos(M_PI/3);
+                    moveArm(xd, Rd, command_torques, robot, control_point);
+                    Vector3d x;
+                    robot->position(x, "link7", control_point);
+                    cout << "x = " << x << endl;
+                    if ((x - xd).norm() < 0.1) {
+                        state = GRAB_MAIL;
+                        cout << "Next: grab mail!!!!" << endl;
+                    }
+                    break;
+                }
+            case GRAB_MAIL:
+                {
+
+                state = PLACE_MAIL;
+                break;
+                }
+            case PLACE_MAIL:
+                {
+
+                state = CLOSE_BOX;
+                break;
+                }
+            case CLOSE_BOX:
+                {
+
+                state = RETRACT_ARM;
+                break;
+                }
+            case RETRACT_ARM:
+                {
+
+                state = WAIT_FOR_BOX;
+                break;
+                }
+            default:
+                {
+                //shouldn't be stateless
+                cout << "I am stateless :(" << endl;
+                }
+        }
+
+/*	
 		if(state == JOINT_CONTROLLER)
 		{
 			// update task model and set hierarchy
@@ -158,8 +240,12 @@ int main() {
 			posori_task->computeTorques(posori_task_torques);
 			joint_task->computeTorques(joint_task_torques);
 
-			command_torques = posori_task_torques + joint_task_torques;
+			//command_torques = posori_task_torques + joint_task_torques;
+			command_torques = posori_task_torques;
+            for (int i = 1; i < 11; i++) command_torques(i) = 0;
+            cout << command_torques << endl;
 		}
+*/
 
 		// send to redis
 		redis_client.setEigenMatrixJSON(JOINT_TORQUES_COMMANDED_KEY, command_torques);
@@ -174,4 +260,82 @@ int main() {
     std::cout << "Controller Loop frequency : " << timer.elapsedCycles()/end_time << "Hz\n";
 
 	return 0;
+}
+
+
+void moveTruck(VectorXd q_desired, VectorXd &command_torques, Sai2Model::Sai2Model* &robot) {
+    double kp = 100;
+    double kv = 20;
+    VectorXd g(robot->dof());
+    robot->gravityVector(g);
+    VectorXd b(robot->dof());
+    robot->coriolisForce(b);
+    command_torques = robot->_M * (-kp * (robot->_q - q_desired) - kv * robot->_dq) + b + g;  
+}
+
+// Rd is direction cosines
+void moveArm(VectorXd xd, Matrix3d Rd, VectorXd &command_torques, Sai2Model::Sai2Model* &robot, Vector3d pos_in_link) {
+    string link_name = "link7";
+    Vector3d x, x_dot;
+    robot->position(x, link_name, pos_in_link);
+    robot->linearVelocity(x_dot, link_name, pos_in_link);
+    MatrixXd J;
+    robot->J_0(J, link_name, pos_in_link);
+    // chop off truck joints
+    MatrixXd J_bot = J.block(0,3,6,7);
+
+    MatrixXd J_bar, Lambda, N;
+    operationalSpaceMatrices(Lambda, J_bar, N, J_bot, robot);
+    VectorXd g(robot->dof());
+    robot->gravityVector(g);
+    Matrix3d R;
+    robot->rotation(R, link_name);
+    
+    Vector3d d_phi = Vector3d(0,0,0);
+    for (int i = 0; i < 3; i++) {
+        d_phi -= 0.5 * R.col(i).cross(Rd.col(i));
+    }
+
+    Vector3d omega;
+    robot->angularVelocity(omega, link_name);
+    double kp = 100;
+    double kv = 20;
+    double kvj = 14;
+
+    VectorXd Fv = kp * (xd - x) - kv * x_dot;
+    VectorXd Fw = kp * (-d_phi) - kv * omega;
+    VectorXd F(Fv.size() + Fw.size());
+    F << Fv, Fw; 
+    F = Lambda * F;
+    command_torques << 0,0,0,J_bot.transpose() * F - N.transpose() * kvj * robot->_dq;
+    command_torques += g;
+}
+
+
+void operationalSpaceMatrices(MatrixXd& Lambda, MatrixXd& Jbar, MatrixXd& N,
+                                    const MatrixXd& task_jacobian, Sai2Model::Sai2Model* &robot)
+{
+    MatrixXd N_prec = MatrixXd::Identity(7,7);
+
+	auto t1 = chrono::high_resolution_clock::now();
+	auto t2 = chrono::high_resolution_clock::now();
+	double duration = 0;
+
+	t1 = chrono::high_resolution_clock::now();
+
+	// resize matrices
+	int k = task_jacobian.rows();
+	Lambda.setZero(k,k);
+	Jbar.setZero(7,k);
+	N.setIdentity(7,7);
+
+    MatrixXd _M_inv = robot->_M_inv.block(3,3,7,7);
+
+	// Compute the matrices
+	MatrixXd inv_inertia = task_jacobian*_M_inv*task_jacobian.transpose();
+	Lambda = inv_inertia.llt().solve(MatrixXd::Identity(k,k));
+	Jbar = _M_inv*task_jacobian.transpose()*Lambda;
+	MatrixXd Ni = MatrixXd::Identity(7,7);
+	N = MatrixXd::Identity(7,7) - Jbar*task_jacobian;
+	N = N*N_prec;
 }
