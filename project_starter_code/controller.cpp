902ec2b451207959cdb1253b168eb6302ad96c25
Kevin Supakkul <kev.supakkul@gmail.com>
2021-05-13 01:04:57 -0500

    posture control

--- project_starter_code/controller.cpp
+++ project_starter_code/controller.cpp
@@ -27,7 +27,8 @@ const string robot_file = "./resources/mmp_panda.urdf";
 //int state = POSORI_CONTROLLER;
 
 void moveTruck(VectorXd q_desired, VectorXd &command_torques, Sai2Model::Sai2Model* &robot, double drive_time);
-void moveArm(VectorXd xd, Matrix3d &Rd, VectorXd &command_torques, Sai2Model::Sai2Model* &robot, Vector3d pos_in_link);
+//void moveArm(VectorXd xd, Matrix3d &Rd, VectorXd &command_torques, Sai2Model::Sai2Model* &robot, Vector3d pos_in_link);
+void moveArm(VectorXd xd, Matrix3d &Rd, VectorXd &qd, VectorXd &command_torques, Sai2Model::Sai2Model* &robot, Vector3d pos_in_link);
 void operationalSpaceMatrices(MatrixXd& Lambda, MatrixXd& Jbar, MatrixXd& N, const MatrixXd& task_jacobian, Sai2Model::Sai2Model* &robot);
 
 typedef enum {INIT, WAIT_FOR_BOX, SCAN_FOR_BOX, OPEN_BOX, GRAB_MAIL, PLACE_MAIL, CLOSE_BOX, RETRACT_ARM} States;
@@ -175,10 +176,18 @@ int main() {
             }
             case OPEN_BOX:
             {
-                Vector3d xd = Vector3d(6, 0.5, 0.8);
+                //Vector3d xd = Vector3d(6, 0.5, 0.7);
+                Vector3d xd = Vector3d(5.4, -0.04, 0.6);
                 Matrix3d Rd;
-                Rd << cos(M_PI/3),0,sin(M_PI/3),0,1,0,-sin(M_PI/3),0,cos(M_PI/3);
-                moveArm(xd, Rd, command_torques, robot, control_point);
+                //Rd << cos(M_PI/3),0,sin(M_PI/3),0,1,0,-sin(M_PI/3),0,cos(M_PI/3);
+                //Rd << 1,0,0,0,0,-1,0,1,0;
+                //Rd <<  0.598127, 0.560538, 0.572749, -0.761619, 0.175214, 0.623888, 0.249359, -0.809381, 0.531717;
+                //Rd << 0.706073, 0.707504, -0.0299898, 0.377976,  -0.340723,   0.860838,  0.598828,   -0.61915,  -0.507995;
+                //Rd << 0.7387, -0.512564,  0.437723, -0.427289,  0.146146,  0.892225,-0.521294,  -0.84612, -0.111055;
+                Rd << 1,0,0,0,1,0,0,0,1;
+                VectorXd qd = initial_q.segment(3,9);
+                qd(0) += M_PI;
+                moveArm(xd, Rd, qd, command_torques, robot, control_point);
                 Vector3d x;
                 robot->position(x, "link7", control_point);
                 if ((x - xd).norm() < 0.1) {
@@ -196,12 +205,20 @@ int main() {
                 q_desired(10) = 0;
                 q_desired(11) = 0;
                 command_torques = robot->_M * (-kp * (robot->_q - q_desired) - kv * robot->_dq);  
+
+                
+                Matrix3d R;
+                robot->rotation(R, "link7");
+                cout << "R = " << R << endl;
+                Vector3d x;
+                robot->position(x, "link7", control_point);
+                cout << "x = " << x << endl;
                
                 // redefine state trigger later 
                 if ((robot->_q.tail<2>() - q_desired.tail<2>()).norm() < 0.1
                                 && robot->_dq.norm() < 0.01) {
                     cout << "Gripper closed!!!!" << endl;
-                    state = PLACE_MAIL;
+                    //state = PLACE_MAIL;
                     q_desired = robot->_q;
                 }
                 break;
@@ -322,7 +339,7 @@ void moveTruck(VectorXd q_desired, VectorXd &command_torques, Sai2Model::Sai2Mod
 }
 
 // Rd is direction cosines
-void moveArm(VectorXd xd, Matrix3d &Rd, VectorXd &command_torques, Sai2Model::Sai2Model* &robot, Vector3d pos_in_link) {
+void moveArm(VectorXd xd, Matrix3d &Rd, VectorXd &qd, VectorXd &command_torques, Sai2Model::Sai2Model* &robot, Vector3d pos_in_link) {
     string link_name = "link7";
     Vector3d x, x_dot;
     robot->position(x, link_name, pos_in_link);
@@ -346,14 +363,17 @@ void moveArm(VectorXd xd, Matrix3d &Rd, VectorXd &command_torques, Sai2Model::Sa
     robot->angularVelocity(omega, link_name);
     double kp = 100;
     double kv = 20;
-    double kvj = 14;
+    double kpj = 200;
+    double kvj = 50;
 
     VectorXd Fv = kp * (xd - x) - kv * x_dot;
     VectorXd Fw = kp * (-d_phi) - kv * omega;
     VectorXd F(Fv.size() + Fw.size());
     F << Fv, Fw; 
     F = Lambda * F;
-    command_torques << 0,0,0,J_bot.transpose() * F - N.transpose() * kvj * robot->_dq;
+    command_torques << 0,0,0,J_bot.transpose() * F - N.transpose() * kvj * robot->_dq.segment(3,9);
+    MatrixXd M7 = robot->_M.block(3,3,7,7);
+    command_torques << 0,0,0,J_bot.transpose() * F + N.transpose() * M7 * (-kpj * (robot->_q.segment(3,9) - qd) - kvj * robot->_dq.segment(3,9));
 
     // note: simviz.cpp already adds gravity vector
     //VectorXd g(robot->dof());
